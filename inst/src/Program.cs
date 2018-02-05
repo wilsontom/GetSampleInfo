0c9f91719d5f419a1178f31727d8cc6c738fad1a
wilsontom <wilsontom89@gmail.com>
2018-02-05 10:21:39 +0000

    update exe

---
+++ inst/src/Program.cs
@@ -0,0 +1,76 @@
+﻿// Tom Wilson <tpw2@aber.ac.uk>
+//
+// 2018-02-05
+//
+// Access Sample Information
+
+namespace RawFileReaderGetSampleInfo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Runtime.ExceptionServices;
+
+    using ThermoFisher.CommonCore.Data;
+    using ThermoFisher.CommonCore.Data.Business;
+//  using ThermoFisher.CommonCore.Data.FilterEnums;
+//  using ThermoFisher.CommonCore.Data.Interfaces;
+//  using ThermoFisher.CommonCore.MassPrecisionEstimator;
+    using ThermoFisher.CommonCore.RawFileReader;
+
+internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string filename = string.Empty;
+
+            if (args.Length > 0)
+            {
+                filename = args[0];
+            }
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                Console.WriteLine("No RAW file specified!");
+
+                return;
+            }
+
+            // Create the IRawDataPlus object for accessing the RAW file
+            var rawFile = RawFileReaderAdapter.FileFactory(filename);
+
+            if (!rawFile.IsOpen || rawFile.IsError)
+            {
+                Console.WriteLine("Unable to access the RAW file using the RawFileReader class!");
+
+                return;
+            }
+
+            rawFile.SelectInstrument(Device.MS, 1);
+
+            // Get information related to the sample that was processed
+            Console.WriteLine("sample_type -- " + rawFile.SampleInformation.SampleType);
+            Console.WriteLine("file_name -- " + rawFile.SampleInformation.RawFileName);
+            Console.WriteLine("sample_id -- " + rawFile.SampleInformation.Path);
+            Console.WriteLine("instrument_method -- " + rawFile.SampleInformation.InstrumentMethodFile);
+            Console.WriteLine("process_method -- " + rawFile.SampleInformation.ProcessingMethodFile);
+            Console.WriteLine("positiion -- " + rawFile.SampleInformation.Vial);
+            Console.WriteLine("inj_vol -- " + rawFile.SampleInformation.InjectionVolume);
+            Console.WriteLine("level -- " + rawFile.SampleInformation.CalibrationLevel);
+            Console.WriteLine("sample_wt -- " + rawFile.SampleInformation.SampleWeight);
+            Console.WriteLine("sample_vol -- " + rawFile.SampleInformation.SampleVolume);
+            Console.WriteLine("istd_amount -- " + rawFile.SampleInformation.IstdAmount);
+            Console.WriteLine("dilution -- " + rawFile.SampleInformation.DilutionFactor);
+            Console.WriteLine("comment -- " + rawFile.SampleInformation.Comment);
+            Console.WriteLine("sample_name -- " + rawFile.SampleInformation.SampleName);
+            Console.WriteLine("batch -- " + rawFile.SampleInformation.UserText[0]);
+            Console.WriteLine("block -- " + rawFile.SampleInformation.UserText[1]);
+            Console.WriteLine("class -- " + rawFile.SampleInformation.UserText[2]);
+            Console.WriteLine("inj_order -- " + rawFile.SampleInformation.UserText[3]);
+            Console.WriteLine("sample_order -- " + rawFile.SampleInformation.UserText[4]);
+
+        }
+    }
+
+}
