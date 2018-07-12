ed768d9daab10bbca7ffef95f0cfbeca72ef280d
wilsontom <wilsontom89@gmail.com>
2018-07-12 10:17:27 +0100

    src code for scan filter extraction

---
+++ inst/src/GetScanFilters.cs
@@ -0,0 +1,61 @@
+// Tom Wilson <tpw2@aber.ac.uk>
+//
+// 2018-07-11
+//
+// Access Sample Scan Filters
+
+namespace RawFileReaderGetScanFilters
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Diagnostics;
+  using System.IO;
+  using System.Runtime.ExceptionServices;
+
+  using ThermoFisher.CommonCore.Data;
+  using ThermoFisher.CommonCore.Data.Business;
+  //  using ThermoFisher.CommonCore.Data.FilterEnums;
+  //  using ThermoFisher.CommonCore.Data.Interfaces;
+  //  using ThermoFisher.CommonCore.MassPrecisionEstimator;
+  using ThermoFisher.CommonCore.RawFileReader;
+
+  internal static class Program
+  {
+    private static void Main(string[] args)
+    {
+      string filename = string.Empty;
+
+      if (args.Length > 0)
+      {
+        filename = args[0];
+      }
+
+      if (string.IsNullOrEmpty(filename))
+      {
+        Console.WriteLine("No RAW file specified!");
+
+        return;
+      }
+
+      // Create the IRawDataPlus object for accessing the RAW file
+      var rawFile = RawFileReaderAdapter.FileFactory(filename);
+
+      if (!rawFile.IsOpen || rawFile.IsError)
+      {
+        Console.WriteLine("Unable to access the RAW file using the RawFileReader class!");
+
+        return;
+      }
+
+      rawFile.SelectInstrument(Device.MS, 1);
+
+      // Get information related to the sample that was processed
+       var scanFilters = rawFile.GetAutoFilters();
+        foreach (var filter in scanFilters)
+                {
+                Console.WriteLine(filter);
+                    }
+
+    }
+  }
+}
