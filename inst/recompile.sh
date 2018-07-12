95c10f93a32683a292acbc269692d1f25df935b6
wilsontom <wilsontom89@gmail.com>
2018-07-12 10:18:15 +0100

    add new src to recompiler

--- inst/recompile.sh
+++ inst/recompile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mcs src/GetSampleInfo.cs -r:bin/RawFileReader/ThermoFisher.CommonCore.Data.dll -r:bin/RawFileReader/ThermoFisher.CommonCore.RawFileReader.dll -out:bin/RawFileReader/GetSampleInfo.exe
+mcs src/GetScanFilters.cs -r:bin/RawFileReader/ThermoFisher.CommonCore.Data.dll -r:bin/RawFileReader/ThermoFisher.CommonCore.RawFileReader.dll -out:bin/RawFileReader/GetScanFilters.exe
