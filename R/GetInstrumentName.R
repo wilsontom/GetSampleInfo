55dc840bcbd796bfbd8aacb06b53a9849ea36a44
wilsontom <wilsontom89@gmail.com>
2018-07-28 11:20:19 +0100

    R wrapper for new src

---
+++ R/GetInstrumentName.R
@@ -0,0 +1,31 @@
+#' Get Instrument Name
+#'
+#' Extract instrumment identification fields
+#'
+#' @param x a \code{.raw} file
+#' @return a \code{list} of four elements
+#' @export
+
+GetInstrumentName <- function(x)
+{
+  if (tolower(tools::file_ext(x)) != 'raw') {
+    stop('input must be a .raw file')
+  }
+
+  raw_file_read <-
+    system.file('bin/RawFileReader/GetInstrumentName.exe', package = 'GetSampleInfo')
+
+  input_cmd <- paste(raw_file_read, x, sep = ' ')
+
+  cmd_res <- as.list(system(input_cmd, intern = TRUE))
+
+  res_split <- lapply(cmd_res, function(x)
+    (strsplit(x, ' -- ')))
+
+  list_names <- sapply(res_split, function(x){x[[1]][1]})
+  list_values <- lapply(res_split, function(x){x[[1]][2]})
+
+  names(list_values) <- list_names
+
+  return(list_values)
+}
