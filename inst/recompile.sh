4b7f0eba4274605be70ea2ab6995a7e9999e46df
wilsontom <wilsontom89@gmail.com>
2018-07-28 11:21:44 +0100

    update recompile script

--- inst/recompile.sh
+++ inst/recompile.sh
@@ -2,3 +2,5 @@
 
 mcs src/GetSampleInfo.cs -r:bin/RawFileReader/ThermoFisher.CommonCore.Data.dll -r:bin/RawFileReader/ThermoFisher.CommonCore.RawFileReader.dll -out:bin/RawFileReader/GetSampleInfo.exe
 mcs src/GetScanFilters.cs -r:bin/RawFileReader/ThermoFisher.CommonCore.Data.dll -r:bin/RawFileReader/ThermoFisher.CommonCore.RawFileReader.dll -out:bin/RawFileReader/GetScanFilters.exe
+mcs src/GetInstrumentName.cs -r:bin/RawFileReader/ThermoFisher.CommonCore.Data.dll -r:bin/RawFileReader/ThermoFisher.CommonCore.RawFileReader.dll -out:bin/RawFileReader/GetInstrumentName.exe
+
