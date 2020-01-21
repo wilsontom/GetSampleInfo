0b5dc42c38162e63ee78d77a74c1f9640d2dc342
wilsontom <wilsontom89@gmail.com>
2020-01-21 16:58:54 +0000

    remvoe testing for now

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(GetSampleInfo)
-
-test_check('GetSampleInfo')
--- tests/testthat/test-get_info.R
+++
@@ -1,9 +0,0 @@
-context('get-sample-info')
-
-test_that('sample-info', {
-
-  raw_file <- system.file('extdata/QC01.raw', package = 'GetSampleInfo')
-
-  expect_true(is.data.frame(GetSampleInfo(raw_file)))
-
-})
